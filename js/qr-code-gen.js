35b1977ff9a5669cadb6c63646e3c9a33f7107df
PlayingHot <78855143+PlayingHot@users.noreply.github.com>
2024-05-11 14:02:51 +0530

    Login Page Redesign& Functionality
    Login working

--- js/qr-code-gen.js
+++ js/qr-code-gen.js
@@ -1,6 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
     let qrbox=document.getElementById("qrbox");
     let qrcode=document.getElementById("qrcode");
-      qrcode.src= "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Example+Vada_Pav";
-    
+    const qrCodeUrl = generateQRCode(listCard);
+    function generateQRCode(data) {
+      const qrData = JSON.stringify(products);
+      const qrSize = "150x150";
+      const qrUrl = `https://api.qrserver.com/v1/create-qr-code/?size=${qrSize}&data=${qrData}`;
+      return qrUrl;
+  }
+  
+  console.log(qrCodeUrl); // Output: URL of the generated QR code
 });
